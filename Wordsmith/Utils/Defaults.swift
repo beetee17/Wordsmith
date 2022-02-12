76ae20c2118a09f32e841656895aea2bf3ff341e
beetee17 <brandon.thio@yahoo.com.sg>
2022-02-12 17:28:20 +0800

    Remove unnecessary constants

--- Wordsmith/Utils/Defaults.swift
+++ Wordsmith/Utils/Defaults.swift
@@ -15,68 +15,7 @@ struct Device {
 
 struct Global {
     static let dictionary = loadWords("words")
-    static let answers = loadWords("answers")
     static let maxAttempts = 6
-    static let numLetters = 5
-    
-}
-
-struct Player {
-    static var numPlayed: Int {
-        defaults.integer(forKey: "Num Played")
-    }
-    static var currStreak: Int {
-        defaults.integer(forKey: "Current Win Streak")
-    }
-    static var bestStreak: Int {
-        defaults.integer(forKey: "Previous Best")
-    }
-    
-    static var guessHistory: [String : Int] {
-        guard let dict = defaults.object(forKey: "Guess History") as? [String : Int] else {
-            print("Guess history not found")
-            return [String:Int]()
-        }
-        return dict
-    }
-    static var guessDistribution: [String : Int] {
-        guard let dict = defaults.object(forKey: "Num Guesses History") as? [String : Int] else {
-            print("Guess distribution not found")
-            return [String:Int]()
-        }
-        return dict
-    }
-    
-    static func updateGuesses(with guess: String) {
-        var dict = Player.guessHistory
-        
-        if let value = dict[guess] {
-            dict.updateValue(value+1, forKey: guess)
-        } else {
-            print("Did not find \(guess) in \(dict)")
-            dict.updateValue(1, forKey: guess)
-        }
-        defaults.set(dict, forKey: "Guess History")
-        print("Updated key \(guess) to value \(dict[guess])")
-    }
-    static func incrementNumPlayed() {
-        defaults.set(Player.numPlayed+1, forKey: "Num Played")
-    }
-    
-    static func updateGuessDistribution(with numGuesses: Int) {
-        var dict = Player.guessDistribution
-        
-        let key = "\(numGuesses)"
-        
-        if let value = dict[key] {
-            dict.updateValue(value+1, forKey: key)
-        } else {
-            dict.updateValue(1, forKey: key)
-        }
-        defaults.set(dict, forKey: "Num Guesses History")
-        print("Updated key \(key) to value \(dict[key])")
-    }
-    
 }
 
 // Loading of Data
