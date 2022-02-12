6eeeb2c3749f578d2c8c825040cd9d84f7a00990
beetee17 <brandon.thio@yahoo.com.sg>
2022-02-12 17:32:33 +0800

    Add button for user to change wordle length

--- Wordsmith/UI/TopBar.swift
+++ Wordsmith/UI/TopBar.swift
@@ -9,44 +9,60 @@ import SwiftUI
 
 struct TopBar: View {
     @EnvironmentObject var viewModel: WordSmithViewModel
-    @Binding var showStats: Bool
-    @Binding var showTutorial: Bool
-    @Binding var showLeaderboards: Bool
+    @ObservedObject var contentVM: ContentViewModel
     
     var body: some View {
+        
         HStack {
-            Button(action: { withAnimation { showTutorial = true } }) {
-                Image(systemName: "questionmark.circle.fill")
-                    .resizable().scaledToFill()
-                    .frame(width:25, height: 25)
-                    .foregroundColor(.TEXT)
-            }
             
             Spacer()
-            Button(action: { withAnimation { showLeaderboards = true } }) {
-                Text("Win Streak: \(viewModel.currentStreak)")
-                    .foregroundColor(.TEXT)
-                    .font(.system(size: 20, weight: .bold, design: .default))
-            }
+                .frame(width:Device.width*0.1)
+                .overlay(
+                    Button(action: { withAnimation { contentVM.showTutorial = true } }) {
+                        Image(systemName: "questionmark.circle.fill")
+                            .resizable().scaledToFill()
+                            .frame(width:25, height: 25)
+                            .foregroundColor(.TEXT)
+                    }
+                )
             
             Spacer()
-
-            Button(action: { withAnimation { showStats = true } }) {
-                Image("chart.line.uptrend.xyaxis.circle.fill")
-                    .resizable().scaledToFill()
-                    .frame(width:25, height: 25)
-                    .foregroundColor(.TEXT)
-            }
+                .frame(width: Device.width*0.65)
+                .overlay(
+                    Button(action: { withAnimation { viewModel.showLeaderboards = true } }) {
+                        Text("Win Streak: \(viewModel.game.currStreak)")
+                            .foregroundColor(.TEXT)
+                            .font(.system(size: 20, weight: .bold, design: .default))
+                    }
+                )
+            
+            Spacer()
+                .frame(width:Device.width*0.1)
+                .overlay(
+                    HStack(spacing:10) {
+                        Button(action: { withAnimation { contentVM.showStats = true } }) {
+                            Image("chart.line.uptrend.xyaxis.circle.fill")
+                                .resizable().scaledToFill()
+                                .frame(width:25, height: 25)
+                                .foregroundColor(.TEXT)
+                        }
+                        Button(action: { withAnimation { contentVM.showGameModes = true } }) {
+                            Image("line.3.horizontal")
+                                .resizable().scaledToFit()
+                                .frame(width:30, height: 30)
+                                .foregroundColor(.TEXT)
+                        }
+                    }
+                )
         }
-        .padding(.horizontal, 30)
+        .frame(height:30)
+        .buttonStyle(ScaleEffect())
     }
 }
 
 struct TopBar_Previews: PreviewProvider {
     static var previews: some View {
-        TopBar(showStats: .constant(false),
-               showTutorial: .constant(false),
-               showLeaderboards: .constant(false))
+        TopBar(contentVM: ContentViewModel())
             .environmentObject(WordSmithViewModel())
     }
 }
